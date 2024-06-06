c74e27e2b3191b6183db72d956832c78c20e9a23
duplidev <duplicat@mail.com>
2024-06-06 11:38:54 +0200

    add animations & scroll offset

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ window.addEventListener("resize", () => {
         navmenu.style.display = "none";
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+            } else {
+                entry.target.classList.remove("in-view");
+            }
+        });
+    });
+
+    const allAnimatedElements = document.querySelectorAll('.animate');
+
+    allAnimatedElements.forEach((element) => observer.observe(element));
+});
